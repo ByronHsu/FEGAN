bb4b847affd0c14a656a5174ad545b53f614a598
Mckinsey666 <whereisbrianchao520@gmail.com>
2019-09-29 17:57:08 +0800

    fix visulization for no vizdom

--- util/.ipynb_checkpoints/visualizer-checkpoint.py
+++ util/.ipynb_checkpoints/visualizer-checkpoint.py
@@ -4,7 +4,9 @@ import ntpath
 import time
 from . import util
 from . import html
-
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 class Visualizer():
     def __init__(self, opt):
@@ -98,20 +100,27 @@ class Visualizer():
 
     # errors: dictionary of error labels and values
     def plot_current_errors(self, epoch, counter_ratio, opt, errors):
+        if not hasattr(self, 'plot_data'):
+            self.plot_data = {'X': [], 'Y': [], 'legend': list(errors.keys())}
+        self.plot_data['X'].append(epoch + counter_ratio)
+        self.plot_data['Y'].append([errors[k] for k in self.plot_data['legend']])
+        opts={
+                'title': self.name + ' loss over time',
+                'legend': self.plot_data['legend'],
+                'xlabel': 'epoch',
+                'ylabel': 'loss'
+        }
         if self.display_id > 0:
-            if not hasattr(self, 'plot_data'):
-                self.plot_data = {'X': [], 'Y': [], 'legend': list(errors.keys())}
-            self.plot_data['X'].append(epoch + counter_ratio)
-            self.plot_data['Y'].append([errors[k] for k in self.plot_data['legend']])
             self.vis.line(
                 X=np.stack([np.array(self.plot_data['X'])] * len(self.plot_data['legend']), 1),
                 Y=np.array(self.plot_data['Y']),
-                opts={
-                    'title': self.name + ' loss over time',
-                    'legend': self.plot_data['legend'],
-                    'xlabel': 'epoch',
-                    'ylabel': 'loss'},
+                opts=opts,
                 win=self.display_id)
+        else:
+            plt.plot(X=np.stack([np.array(self.plot_data['X'])] * len(self.plot_data['legend']), 1),
+                Y=np.array(self.plot_data['Y']), **opts)
+            plt.savefig('loss.png')
+            
 
     # errors: same format as |errors| of plotCurrentErrors
     def print_current_errors(self, epoch, i, errors, t):
